49617462cd27d786890cc01f42f69b1accf648af
Braden Amorozo <braden808@gmail.com>
2016-11-07 08:11:45 -1000

    update

---
+++ javascript.js
@@ -0,0 +1,111 @@
+// this is a comment
+per line
+
+/* this is a block comment 
+
+comment
+
+*/
+
+Vatiables are like storage containers
+
+// this is a string
+var myName = "Braden";
+
+// this is a integer
+var myAge = 12;	
+
+// this is a boolean
+var booleanExample = true;
+
+// unassigned variable
+undefined
+
+// not a number
+NaN
+
+//Spaces count
+
+var camelCase = "yay"
+
+var firstname = "Braden"
+var lastname = "Amorozo"
+
+console.log(firstname);
+
+var firstname = "Braden"
+var lastname = "Amorozo"
+var age = 12;
+var dylan = "yolo"
+console.log(dylan);
+
+Conditional statements
+
+//compare is ==
+//uses if statement
+commands
+//= is assignment
+//== checks for the equality for two expressions
+
+Boolean Operators
+
+//!==not equals
+
+Logical Operators
+
+// &&-and
+// ||
+
+Functions
+//function is an executed statement
+function (){
+
+}
+
+function nameOfFunction (){
+	console.log("hello")
+}
+
+//invoking a function
+nameOfFunction();
+
+//parameters
+function hello (a,b) {
+	console.log(a+b);
+}
+
+hello(10, 11);
+
+function createGreeting (name, age){
+	if (age < 32){
+		console.log('hello' + name);
+		return 'hello' + name;
+	}else{
+		console.log('hello Mr.' + name)
+		return 'hello Mr.' + name
+		return 'hello MRS.' + name
+	}
+}
+
+var greenAliens = 9000;
+var blueAliens = 500;
+var redAliens = 500;
+var total = greenAliens + blueAliens + redAliens;
+
+if (total == 10000) {
+  console.log("awesome");
+
+   if (greenAliens > 5000){
+  console.log ("why are they so ugly?");
+  }
+
+  if (greenAliens + redAliens > 8000){
+  console.log("no why?????");
+  }
+
+  if (redAliens + blueAliens <= 3000){
+  console.log("okay this seems somewhat manageable, wait no, we're still dead");
+  }
+  
+}
+
